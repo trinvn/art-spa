bfe58743c2a2b49ed5b3647b2c8437dca0a4fe73
TriNV <nvtri.dtu@gmail.com>
2025-04-17 15:32:20 +0000

    Init code

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php 
+/**
+ * Register/enqueue custom scripts and styles
+ */
+add_action( 'wp_enqueue_scripts', function() {
+	// Enqueue your files on the canvas & frontend, not the builder panel. Otherwise custom CSS might affect builder)
+	if ( ! bricks_is_builder_main() ) {
+		wp_enqueue_style( 'bricks-child', get_stylesheet_uri(), ['bricks-frontend'], filemtime( get_stylesheet_directory() . '/style.css' ) );
+	}
+
+  wp_enqueue_style('custom-style', get_stylesheet_directory_uri() . '/assets/css/styles.css', array(), '0.1.0', 'all');
+
+  wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery'), '', true);
+} );
+
+/**
+ * Register custom elements
+ */
+add_action( 'init', function() {
+  $element_files = [
+    __DIR__ . '/elements/title.php',
+  ];
+
+  foreach ( $element_files as $file ) {
+    \Bricks\Elements::register_element( $file );
+  }
+}, 11 );
+
+/**
+ * Add text strings to builder
+ */
+add_filter( 'bricks/builder/i18n', function( $i18n ) {
+  // For element category 'custom'
+  $i18n['custom'] = esc_html__( 'Custom', 'bricks' );
+
+  return $i18n;
+} );
+
